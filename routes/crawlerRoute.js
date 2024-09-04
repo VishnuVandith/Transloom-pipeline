cbb3854a5db8a8d240bbdbae0eb5cb87a11be331
VishnuVandith <vishnuvandith@gmail.com>
2024-09-04 18:42:08 +0530

    Crawler commit

---
+++ routes/crawlerRoute.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { crawlUrls } = require("../controllers/crawlerController.js");
+
+const router = express.Router();
+
+// Route to handle crawling requests
+router.post("/crawl", async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
+
+  try {
+    const urls = await crawlUrls(url);
+    res.json(urls);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while crawling the website" });
+  }
+});
+
+module.exports = router;
